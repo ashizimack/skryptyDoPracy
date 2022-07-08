afbf05c888b80e71f84375edd767c8c57f0ad7d9
Ashizimack <kaibrodowski@gmail.com>
2022-07-08 22:46:48 +0200

    Statystyki dla pracowników

--- generujDane.sh
+++ generujDane.sh
@@ -1,5 +1,5 @@
-ping -c 100 www.google.com | while read endlooop; do echo "$(date +"%T"): $endlooop"; done >> dane
-sed -i '1d' dane
-cat dane | awk '{print $1,$9}' | sed 's/: time=/ /g' >> poprawioneDane
-rm -f dane
+#ping -c 100 www.google.com | while read endlooop; do echo "$(date +"%T"): $endlooop"; done >> dane
+sed -i '1d' /home/kb/latency/03072022
+cat /home/kb/latency/03072022 | awk '{print $1,$9}' | sed 's/: time=/ /g' >> poprawioneDane
+#rm -f dane
 tail -n 1 poprawioneDane | wc -c | xargs -I {} truncate poprawioneDane -s -{}
